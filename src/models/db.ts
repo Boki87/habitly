7a29ec0e5818afea5627828cbcbb8ac1a17ed83c
Boki87 <peric.bojan87@gmail.com>
2023-02-28 15:25:44 +0100

    refactor: add current time as reminderTime for starter habit

--- src/models/db.ts
+++ src/models/db.ts
@@ -2,6 +2,7 @@
 import { Habit } from "@/types/Habit";
 import { HabitEntry } from "@/types/HabitEntry";
 import Dexie, { Table } from "dexie";
+import { format } from "date-fns";
 
 export class HabitlyDB extends Dexie {
   habits!: Table<Habit, number>;
@@ -18,13 +19,14 @@ export class HabitlyDB extends Dexie {
 export const db = new HabitlyDB();
 
 db.on("populate", async () => {
+  const reminderTime = format(new Date(), "HH:mm").toString();
   const habits = await db.habits.add({
     title: "New Habit",
     desc: "Change me, im a starter habit 😬",
     color: "blue",
     freq: 3,
     useReminder: false,
-    reminderTime: "",
+    reminderTime,
     reminderText: "",
   });
 });
